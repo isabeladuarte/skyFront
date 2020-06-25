8cfd16cf582a7dc4bf544f916d5a5619c7cf9423
isabeladuarte <isabeladuartelbg@gmail.com>
2020-06-25 11:12:10 -0300

    update: div class filmes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,33 +4,17 @@ $(document).ready(function () {
         url: "https://sky-frontend.herokuapp.com/movies",
         success: function (json) {
             console.log(json)
+
                $.each(json[2].movies, function (index, movie) {
                    const div = `
-                        <div class="movies">
+                   <div class="slide-show">
+                        <div class="movie">
                             <img src=${movie.images[0].url}>
                         </div>
-                   </div>`;
-                //console.log(movie)
-                   $('#filmes').append(div);
+                   </div>;`;
+               
+               $('.filmes').append(div);
             });
-            
-            /*let filme1 = json[2].movies[0]
-            let filme2 = json[2].movies[1]
-            let filme3 = json[2].movies[2]
-
-            let filmes = `<div class="slide-show">
-               <div class="movie">
-                    <img src=${filme1.images[0].url}>
-               </div>
-               <div class="movie">
-                    <img src=${filme2.images[0].url}>
-               </div>
-               <div class="movie">
-                    <img src=${filme3.images[0].url}>
-               </div>
-            </div>`
-            
-            $('.movies').append(filmes)*/
         },
            error: function (json) {
             showmessage("error", "Erro", "Estamos trabalhando nesse problema")
@@ -43,12 +27,6 @@ function showmessage(icon, title, text){
         icon: icon,
         title: title,
         text: text
-      })
+    })
 }
-
-$('.main-gallery').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
-});
 

fd1834e382428af6a2241f13507b0be1c11475ca
isabeladuarte <isabeladuartelbg@gmail.com>
2020-06-29 15:10:53 -0300

    Refatorando funcao de requisição API

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,23 +3,22 @@ $(document).ready(function () {
         type: "GET",
         url: "https://sky-frontend.herokuapp.com/movies",
         success: function (json) {
-            console.log(json)
-
-               $.each(json[2].movies, function (index, movie) {
-                   const div = `
-                   <div class="slide-show">
-                        <div class="movie">
-                            <img src=${movie.images[0].url}>
-                        </div>
-                   </div>;`;
+        
+            $.each(json[2].movies, function (index, movie) {
+                const div = `
+                <div class="slide-show">
+                    <div class="movie">
+                        <img src=${movie.images[0].url}>
+                    </div>
+                </div>`;
                
-               $('.filmes').append(div);
+                $('.filmes').append(div);
             });
         },
-           error: function (json) {
+        error: function (json) {
             showmessage("error", "Erro", "Estamos trabalhando nesse problema")
-           }
-        });
+        }
+    });
 });
 
 function showmessage(icon, title, text){
